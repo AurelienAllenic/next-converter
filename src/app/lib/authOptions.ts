2bf8ebe87170f8c7ff8aa06d8ab76387b57d170b
AurelienAllenic <aurelien.allenic@gmail.com>
2025-05-19 15:49:43 +0200

    (fix) corrected one error for deployment

--- src/app/lib/authOptions.ts
+++ src/app/lib/authOptions.ts
@@ -1,8 +1,17 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import type { AuthOptions } from "next-auth";
-import dbConnect from "@/app/lib/mongodb";
-import User from "@/app/models/User";
+import dbConnect from "@/app/lib/mongodb"; // chemin à adapter selon ta structure
+import User from "@/app/models/User"; // idem, adapte selon ton fichier User.ts ou User.js
 import bcrypt from "bcryptjs";
+import { ObjectId } from "mongodb";
+
+interface IUser {
+  _id: ObjectId;
+  name: string;
+  email: string;
+  password: string;
+  // autres propriétés si nécessaire
+}
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -12,12 +21,14 @@ export const authOptions: AuthOptions = {
         email: { label: "Email", type: "text" },
         password: { label: "Mot de passe", type: "password" },
       },
-      async authorize(credentials) {
-        await dbConnect();
-
+      async authorize(credentials: Record<string, string> | undefined) {
         if (!credentials?.email || !credentials?.password) return null;
 
-        const user = await User.findOne({ email: credentials.email });
+        await dbConnect();
+
+        const user = (await User.findOne({
+          email: credentials.email,
+        })) as IUser | null;
         if (!user) return null;
 
         const isValid = await bcrypt.compare(
