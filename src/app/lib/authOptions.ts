ad287a1e63f8f0ca7e9537aa3234ff2b282ee1cb
AurelienAllenic <aurelien.allenic@gmail.com>
2025-05-19 15:32:54 +0200

    (fix) corrected one error for deployment

---
+++ src/app/lib/authOptions.ts
@@ -0,0 +1,44 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import type { AuthOptions } from "next-auth";
+import dbConnect from "@/app/lib/mongodb";
+import User from "@/app/models/User";
+import bcrypt from "bcryptjs";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Mot de passe", type: "password" },
+      },
+      async authorize(credentials) {
+        await dbConnect();
+
+        if (!credentials?.email || !credentials?.password) return null;
+
+        const user = await User.findOne({ email: credentials.email });
+        if (!user) return null;
+
+        const isValid = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+        if (!isValid) return null;
+
+        return {
+          id: user._id.toString(),
+          name: user.name,
+          email: user.email,
+        };
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
