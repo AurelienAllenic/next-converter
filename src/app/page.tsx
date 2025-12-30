2458a561684c8fba848949e935ebff8498b1e11a
AurelienAllenic <aurelien.allenic@gmail.com>
2025-12-30 15:46:58 +0100

    (feat) removed useless login path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,15 @@
 "use client";
 
-import { useSession } from "next-auth/react";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import ImageConverter from "@/components/ImageConverter";
-import QrCodeGenerator from "@/components/QrCodeGenerator";
 import Link from "next/link";
 import styles from './page.module.css'
 
 export default function Home() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  console.log("Home - Session:", session, "Status:", status);
-
-  useEffect(() => {
-    console.log("useEffect - Status:", status, "Session:", session);
-    if (status === "unauthenticated") {
-      console.log("Redirection vers /login");
-      router.push("/login");
-    }
-  }, [status, router]);
-
-  if (status === "loading") {
-    console.log("Affichage de l'écran de chargement");
-    return <div className={styles.loadingContainer}><p className={styles.loadingText}>Chargement...</p></div>;
-  }
-
-  if (!session) {
-    console.log("Aucune session, devrait rediriger");
-    return null; // ou un message, on redirige juste
-  }
-
-  console.log("Rendu de ImageConverter");
   return (
     <div className={styles.container_options}>
+      <h2 style={{ color: "white", paddingBottom: "20px" }}>Next General tool</h2>
       <Link href='/image-converter'>Image Converter</Link>
       <Link href='/qr-code-generator'>QrCodeGenerator</Link>
-      <Link href='/video-to-gif' >Video to GIF</Link>
+      <Link href='/video-to-gif'>Video to GIF</Link>
     </div>
   );
 }
